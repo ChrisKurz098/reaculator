259586ae182759b830d30731ad12b65cc06c7b98
CKURZ <ckurz098@gmail.com>
2022-06-28 16:45:35 -0700

    Formatted button positions

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -19,7 +19,7 @@ const Calulator = () => {
         if (input === '()') if (perenthToggle) { input = ')'; setPerenthToggle(0) } else { input = '('; setPerenthToggle(1) };
         if (input === '(' && !isNaN(parseInt(screenState.at(-1)))) input = '*(';
         if (input === '(' && screenState.at(-1) === ')') input = '*(';
-       
+
 
         //check if '.' is pressed and set input accordingly
         if (input === '.' && nums.at(-1).includes('.')) return;
@@ -63,40 +63,40 @@ const Calulator = () => {
             <Screen
                 screenState={screenState}
             />
-            <div>
-                <button type="button" id="7" onClick={updateCalculation}>7</button>
-                <button type="button" id="8" onClick={updateCalculation}>8</button>
-                <button type="button" id="9" onClick={updateCalculation}>9</button>
+            <div id="numPad">
+                <div className="btnRow">
+                    <button type="button" id="7" onClick={updateCalculation}>7</button>
+                    <button type="button" id="8" onClick={updateCalculation}>8</button>
+                    <button type="button" id="9" onClick={updateCalculation}>9</button>
+                </div>
+                <div className="btnRow">
+                    <button type="button" id="4" onClick={updateCalculation}>4</button>
+                    <button type="button" id="5" onClick={updateCalculation}>5</button>
+                    <button type="button" id="6" onClick={updateCalculation}>6</button>
+                </div>
+                <div className="btnRow">
+                    <button type="button" id="1" onClick={updateCalculation}>1</button>
+                    <button type="button" id="2" onClick={updateCalculation}>2</button>
+                    <button type="button" id="3" onClick={updateCalculation}>3</button>
+                </div>
+                <div className="btnRow">
+                    <button type="button" id="0" onClick={updateCalculation}>0</button>
+                    <button type="button" id="." onClick={updateCalculation}>.</button>
+                    <button type="button" id="DEL" onClick={deleteLast}>BK</button>
+                </div>
             </div>
-            <div>
-                <button type="button" id="4" onClick={updateCalculation}>4</button>
-                <button type="button" id="5" onClick={updateCalculation}>5</button>
-                <button type="button" id="6" onClick={updateCalculation}>6</button>
-            </div>
-            <div>
-                <button type="button" id="1" onClick={updateCalculation}>1</button>
-                <button type="button" id="2" onClick={updateCalculation}>2</button>
-                <button type="button" id="3" onClick={updateCalculation}>3</button>
-            </div>
-            <div>
-                <button type="button" id="0" onClick={updateCalculation}>0</button>
-                <button type="button" id="." onClick={updateCalculation}>.</button>
-                <button type="button" id="DEL" onClick={deleteLast}>BK</button>
-            </div>
-            <div>
+            <div id="btnInputs">
                 <button type="button" id="+" onClick={updateCalculation}>+</button>
                 <button type="button" id="-" onClick={updateCalculation}>-</button>
                 <button type="button" id="/" onClick={updateCalculation}>/</button>
                 <button type="button" id="*" onClick={updateCalculation}>*</button>
                 <button type="button" id="()" onClick={updateCalculation}>( )</button>
                 <button type="button" id="=" onClick={calculateValue}>=</button>
-
             </div>
+            <div id="clrrcl">
+            <button type="button" id="RCL" onClick={getPrevEqu}>RCL</button>
             <button type="button" id="CLR" onClick={() => setScreenState([0])}>CLR</button>
-            <button type="button" id="CLR" onClick={getPrevEqu}>RECALL</button>
-
-
-
+            </div>
         </div>
     )
 }
